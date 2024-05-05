5924c2ca4c6676560ef2f5eb6b51eec79b210b4d
Himanshu Choudhary <Hchoudhary525@gmail.com>
2024-05-05 11:23:34 +0530

    Version 2

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class LoanPrediction(BaseModel):
 
 @app.get("/")
 def index():
-    return {"message": "Welcome to the loan prediction app using jenkins"}
+    return {"message": "Welcome to the loan prediction app using jenkins: Version 2.0"}
 
 
 @app.post("/predict_api")
